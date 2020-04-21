0a5d53e310c3b4b6ab41871aa871fb90d1e6d351
armand <armanddzoyem@gmail.com>
2020-04-21 10:47:20 +0000

    bonjour

---
+++ addTwolnt.py
@@ -0,0 +1,22 @@
+#!/usr/bin//env python3
+#dzoyem meli armand
+#addition
+
+import sys
+
+def add(a,b):
+
+       return a + b
+
+if __name__ == "__main__" :
+
+         if len(sys.argv) > 3 :
+             print("on a besoin de deux arguments")
+
+         elif (len(sys.argv) == 3) :
+            a = int(sys.argv[1])
+            b = int(sys.argv[2])
+            print(add(a,b))
+
+         else:
+            print("les arguments sont inssuffissants")
